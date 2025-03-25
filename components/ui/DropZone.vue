9df12093cd5e376690a7ebe78b7fc07233c65519
Tom Radford <tom@theradford.com>
2025-03-25 22:01:09 +0200

    fix: reset file input on select in case the user didnt select a file

--- components/ui/DropZone.vue
+++ components/ui/DropZone.vue
@@ -1,32 +1,38 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, useTemplateRef } from "vue";
 import UploadIcon from "../icons/UploadIcon.vue";
 
 const emit = defineEmits<{ fileSelected: [file: File] }>();
 
 const isDraggingOver = ref(false);
+const fileInput = useTemplateRef("file-input");
 
 const handleFileUpload = (event: Event) => {
   const file = (event.target as HTMLInputElement)?.files?.[0];
 
   if (file) {
     emit("fileSelected", file);
+    if (fileInput.value) {
+      fileInput.value.value = "";
+    }
   }
 };
 </script>
 
 <template>
   <div
-    class="w-full h-[350px] hover:cursor-pointer relative flex flex-col items-center justify-center rounded-3xl bg-white/4 hover:bg-white/10 transition-colors outline-dashed outline-2 outline-white/30"
+    class="w-full p-8 md:h-[350px] hover:cursor-pointer relative flex flex-col items-center justify-center rounded-3xl bg-white/4 hover:bg-white/10 transition-colors outline-dashed outline-2 outline-white/30"
     :class="{ 'bg-white/10': isDraggingOver }"
   >
     <div class="flex flex-col items-center gap-3.5">
       <UploadIcon />
-      <p class="text-sm font-light">
+
+      <p class="text-sm hidden sm:block font-light">
         {{ isDraggingOver ? "Drop" : "Upload" }} a CSV of your bank statement
       </p>
     </div>
     <input
+      ref="file-input"
       accept=".csv"
       type="file"
       class="absolute inset-0 opacity-0 cursor-pointer"
