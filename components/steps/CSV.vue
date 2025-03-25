822af975f03f1e79c8f79c7e286f43b2a8decc45
Tom Radford <tom@theradford.com>
2025-03-25 20:18:31 +0200

    wip: CSV step

---
+++ components/steps/CSV.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { useFlowStore } from "~/stores/flow";
+import DropZone from "../ui/DropZone.vue";
+
+const flowStore = useFlowStore();
+</script>
+
+<template>
+  <div class="flex gap-8 flex-col h-full justify-center p-8">
+    <h1 class="text-4xl font-bold">Drop that CSV 🫳</h1>
+    <p>
+      You’re moments away from getting a tasty
+      <span class="font-bold">personal income statement</span>
+    </p>
+    <div class="flex flex-col gap-2">
+      <p class="font-extralight text-xs text-right pr-4">
+        Download an
+        <a href="/examples/flowvue-example.csv" class="font-bold"> example</a>
+      </p>
+      <DropZone
+        @file-selected="
+          (f) => {
+            console.log(f);
+          }
+        "
+      />
+    </div>
+    <div class="">
+      <p class="text-center text-sm font-light">
+        Your money secrets are safe with us 🤫
+      </p>
+    </div>
+  </div>
+</template>
