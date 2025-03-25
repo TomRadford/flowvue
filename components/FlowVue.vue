5448ef354bffb59d4ea7d586972e3362bfa0c9a7
Tom Radford <tom@theradford.com>
2025-03-25 20:17:27 +0200

    feat: update FlowVue to render components dynamically suing our flowStore

--- components/FlowVue.vue
+++ components/FlowVue.vue
@@ -1,24 +1,24 @@
 <script setup lang="ts">
-// FlowVue entry point
-import { ref } from "vue";
-const count = ref(0);
+import { useFlowStore } from "~/stores/flow";
+import AppCard from "./ui/AppCard.vue";
+
+/**
+ * FlowVue entry point ✨
+ *
+ * Used in Nuxt and Web Component 😎
+ *
+ **/
+
+const flowStore = useFlowStore();
 </script>
 
 <template>
-  <div
-    class="w-full p-10 relative flex justify-center items-center h-[500px] sm:w-[700px] sm:h-[800px] overflow-hidden bg-black rounded-3xl"
+  <AppCard
+    :color="flowStore.currentStep.backgroundColor"
+    :position="flowStore.currentStep.backgroundPosition"
   >
-    <div class="flex flex-col items-center justify-center z-10">
-      <span class="text-8xl animate-wiggle mb-8">👋</span>
-      <div class="flex gap-8 flex-col">
-        <h1 class="text-4xl font-bold">Finance pit stop?</h1>
-        <p>Get your income breakdown, pain-free.</p>
-      </div>
-    </div>
-    <div
-      class="absolute bottom-0 w-[500px] h-[500px] -right-1/4 bg-green blur-[300px] z-0"
-    />
-  </div>
+    <component :is="flowStore.currentStep.component" />
+  </AppCard>
 </template>
 
 <style scoped>
