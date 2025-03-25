fcf8727cdd60899c98764243dc79ca14cca62d66
Tom Radford <tom@theradford.com>
2025-03-25 20:15:56 +0200

    feat: Button

---
+++ components/ui/Button.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineEmits<{ click: (payload: MouseEvent) => void }>();
+defineProps<{
+  label: string;
+}>();
+</script>
+
+<template>
+  <button
+    class="bg-black/90 hover:cursor-pointer border-2 rounded-lg py-3 px-5 border-aqua/20 hover:border-aqua/40 transition-[border-color]"
+    @click="$emit('click')"
+  >
+    {{ label }}
+  </button>
+</template>
