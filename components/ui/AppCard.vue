86d544540bf6ab2362a894cd2d00cd77954da223
Tom Radford <tom@theradford.com>
2025-03-25 20:16:44 +0200

    feat: AppCard wrapper

---
+++ components/ui/AppCard.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+const colors = {
+  blue: "bg-blue",
+  green: "bg-green",
+  aqua: "bg-aqua",
+  orange: "bg-orange",
+};
+
+// Instead of using Tailwind classes for positioning, we'll use style bindings
+const positions = {
+  "bottom-right": { bottom: "0px", left: "75%" },
+  "bottom-left": { bottom: "0px", left: "0%" },
+  "top-right": { top: "0px", left: "75%" },
+  "top-left": { top: "0px", left: "0%" },
+};
+
+export type Color = keyof typeof colors;
+export type Position = keyof typeof positions;
+
+withDefaults(
+  defineProps<{
+    color?: Color;
+    position?: Position;
+  }>(),
+  { color: "green", position: "bottom-right" }
+);
+</script>
+
+<template>
+  <div
+    class="w-full p-10 relative flex justify-center items-center h-[500px] sm:w-[700px] sm:h-[800px] overflow-hidden bg-black rounded-3xl"
+  >
+    <div class="w-full h-full z-10">
+      <slot />
+    </div>
+    <div
+      class="absolute w-[500px] h-[500px] duration-500 blur-[300px] z-0 transition-all"
+      :class="[colors[color]]"
+      :style="positions[position]"
+    />
+  </div>
+</template>
