d050cd7228dfa9355c81384266709e99d0e9360d
Tom Radford <tom@theradford.com>
2025-03-29 18:56:56 +0200

    feat: loading component for slow module loading

--- constants/flow.ts
+++ constants/flow.ts
@@ -1,8 +1,27 @@
 import { defineAsyncComponent } from "vue";
+import LoaderPage from "../components/LoaderPage.vue";
+
+const createComponentWithLoader = (
+  // bit of a type hack - to say its a promise that resolve a module import
+  importFn: () => Promise<typeof import("../components/FlowVue.vue")>
+) =>
+  defineAsyncComponent({
+    loader: importFn,
+    // ToDo: Seems like this only works with web component
+    // build. it's probably something I'm misunderstanding
+    // with how Nuxt handles dynamic imports
+    loadingComponent: LoaderPage,
+  });
 
 export const FLOW_COMPONENTS = {
-  welcome: defineAsyncComponent(
+  welcome: createComponentWithLoader(
     () => import("../components/steps/Welcome.vue")
   ),
-  csv: defineAsyncComponent(() => import("../components/steps/CSV.vue")),
+  csv: createComponentWithLoader(() => import("../components/steps/CSV.vue")),
+  income: createComponentWithLoader(
+    () => import("../components/steps/Income.vue")
+  ),
+  expenses: createComponentWithLoader(
+    () => import("../components/steps/Expenses.vue")
+  ),
 };
