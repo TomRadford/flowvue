bd0b68339303f6cc6bc2c6ac8e242e5a16acd3fd
Tom Radford <tom@theradford.com>
2025-03-29 13:51:41 +0200

    feat: Spinner and Loader

---
+++ components/ui/LoaderPage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Spinner from "../icons/Spinner.vue";
+</script>
+
+<template>
+  <div class="w-full h-full flex items-center justify-center">
+    <Spinner />
+  </div>
+</template>
