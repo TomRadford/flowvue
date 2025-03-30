f1cca563fc18214e485549aa979f2bd37c84b0c6
Tom Radford <tom@theradford.com>
2025-03-30 10:59:28 +0200

    feat: add BG_COLORS and TEXT_COLORS constants for centralized color management

---
+++ constants/color.ts
@@ -0,0 +1,15 @@
+export const BG_COLORS = {
+  blue: "bg-blue",
+  green: "bg-green",
+  aqua: "bg-aqua",
+  orange: "bg-orange",
+  purple: "bg-purple",
+};
+
+export const TEXT_COLORS = {
+  blue: "text-blue",
+  green: "text-green",
+  aqua: "text-aqua",
+  orange: "text-orange",
+  purple: "text-purple",
+};
