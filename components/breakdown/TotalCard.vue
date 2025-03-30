d9445150fb6552138bd2610191d1b9927954e859
Tom Radford <tom@theradford.com>
2025-03-30 10:58:08 +0200

    feat: add TotalCard component for displaying formatted amounts with labels

---
+++ components/breakdown/TotalCard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { TEXT_COLORS } from "~/constants/color";
+import type { Color } from "~/types/colors";
+import { formatZAR } from "~/utils/currency";
+
+defineProps<{ label: string; amount: number; color: Color }>();
+</script>
+
+<template>
+  <div
+    class="bg-[#313131]/40 backdrop-blur-2xl rounded-lg flex flex-col gap-1 py-4 px-5"
+  >
+    <h4 class="text-sm">{{ label }}</h4>
+    <p class="text-xl font-semibold" :class="[TEXT_COLORS[color]]">
+      {{ formatZAR(amount) }}
+    </p>
+  </div>
+</template>
