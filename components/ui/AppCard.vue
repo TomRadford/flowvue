3f8d2bd71dc5957bdd78bf44193ffa87bebf6199
Tom Radford <tom@theradford.com>
2025-03-30 10:58:47 +0200

    refactor: replace color definitions with centralized BG_COLORS constant and add id and className props to AppCard component

--- components/ui/AppCard.vue
+++ components/ui/AppCard.vue
@@ -1,11 +1,6 @@
 <script setup lang="ts">
-const colors = {
-  blue: "bg-blue",
-  green: "bg-green",
-  aqua: "bg-aqua",
-  orange: "bg-orange",
-  purple: "bg-purple",
-};
+import { BG_COLORS } from "~/constants/color";
+import type { Color } from "~/types/colors";
 
 // Instead of using Tailwind classes for positioning, we'll use style bindings
 const positions = {
@@ -15,28 +10,31 @@ const positions = {
   "top-left": { top: "0px", left: "0%" },
 };
 
-export type Color = keyof typeof colors;
 export type Position = keyof typeof positions;
 
 withDefaults(
   defineProps<{
     color?: Color;
     position?: Position;
+    id?: string;
+    className?: string;
   }>(),
-  { color: "green", position: "bottom-right" }
+  { color: "green", position: "bottom-right", id: "app-card", className: "" }
 );
 </script>
 
 <template>
   <div
+    :id="id"
     class="w-full p-2 sm:p-10 relative flex justify-center items-center h-[700px] sm:w-[700px] sm:h-[800px] overflow-hidden bg-black sm:rounded-3xl"
+    :class="className"
   >
     <div class="w-full h-full z-10">
       <slot />
     </div>
     <div
       class="absolute w-[500px] h-[500px] duration-500 blur-[300px] z-0 transition-all"
-      :class="[colors[color]]"
+      :class="[BG_COLORS[color]]"
       :style="positions[position]"
     />
   </div>
