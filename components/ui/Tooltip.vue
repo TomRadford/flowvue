42c5a2080cf84bb54abb966b0923b38375cfc4fb
Tom Radford <tom@theradford.com>
2025-03-30 10:59:17 +0200

    chore: disable tooltip portalling for now

--- components/ui/Tooltip.vue
+++ components/ui/Tooltip.vue
@@ -2,36 +2,40 @@
 import {
   TooltipArrow,
   TooltipContent,
-  TooltipPortal,
+  // TooltipPortal,
   TooltipProvider,
   TooltipRoot,
   TooltipTrigger,
 } from "reka-ui";
+// import { inject } from "vue";
 
 defineProps<{ collisionBoundary?: Element }>();
+
+// const portalTarget = inject<string | undefined>("portalTarget") ?? "body";
 </script>
 
 <template>
+  <!-- ToDo mobile alternative for tooltip -->
   <TooltipProvider :delay-duration="0">
     <TooltipRoot>
       <TooltipTrigger class="w-full inline-block truncate">
         <slot name="trigger" />
       </TooltipTrigger>
-      <!-- ToDo portal to parent instead of body -->
-      <TooltipPortal>
-        <TooltipContent
-          class="z-10 select-none rounded-md bg-black/10 backdrop-blur-3xl px-[15px] py-[10px] text-sm leading-none shadow-sm border"
-          :side-offset="5"
-          :collision-boundary="collisionBoundary"
-        >
-          <slot name="content" />
-          <TooltipArrow
-            class="fill-white stroke-gray-200"
-            :width="12"
-            :height="6"
-          />
-        </TooltipContent>
-      </TooltipPortal>
+      <!-- Disabled Teleport since it seems to have issues with web components -->
+      <!-- <TooltipPortal defer :to="portalTarget"> -->
+      <TooltipContent
+        class="z-10 select-none rounded-md bg-black/10 backdrop-blur-3xl px-[15px] py-[10px] text-sm leading-none shadow-sm border border-white"
+        :side-offset="5"
+        :collision-boundary="collisionBoundary"
+      >
+        <slot name="content" />
+        <TooltipArrow
+          class="fill-white stroke-gray-200"
+          :width="12"
+          :height="6"
+        />
+      </TooltipContent>
+      <!-- </TooltipPortal> -->
     </TooltipRoot>
   </TooltipProvider>
 </template>
